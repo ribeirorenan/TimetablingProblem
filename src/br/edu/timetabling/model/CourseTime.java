e1b6653fc7e83a59df870b5214d5154138553d52
ribeirorenan <renanribeiro_13@hotmail.com>
2016-08-01 11:38:25 -0300

    CourseTime e funções adicionais no TimeTable

---
+++ src/br/edu/timetabling/model/CourseTime.java
@@ -0,0 +1,40 @@
+package br.edu.timetabling.model;
+
+/**
+ * Created by renan on 8/1/16.
+ */
+public class CourseTime {
+    private int day;
+    private int periodOfday;
+    private int room;
+
+    public CourseTime(int day, int periodOfday, int room) {
+        this.day = day;
+        this.periodOfday = periodOfday;
+        this.room = room;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getPeriodOfday() {
+        return periodOfday;
+    }
+
+    public void setPeriodOfday(int periodOfday) {
+        this.periodOfday = periodOfday;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+}
