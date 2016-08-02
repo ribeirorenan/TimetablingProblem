8d57835ce3d0749828bdd788642dcc292e55299f
pcmonceff <pcmonceff@gmail.com>
2016-08-02 17:36:19 -0300

    Adicionado restricoes

---
+++ src/br/edu/timetabling/Util/Constraint.java
@@ -0,0 +1,18 @@
+package br.edu.timetabling.Util;
+
+/**
+ * Created by pcmonceff on 02/08/16.
+ */
+public class Constraint {
+    public enum hardConstraint {
+        TEACHER, ROOM, CONFLICTS
+
+    }
+
+    public enum softConstraint{
+        UNAVAIlABILITY
+
+    }
+
+
+}
